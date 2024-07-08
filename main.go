e048ab00f1952f84a69df21fefe822050322a737
vizvasrj <vizvasrj@gmail.com>
2024-07-08 18:14:24 +0530

    DIST

---
+++ main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"time"
+)
+
+func main() {
+	urls := []string{
+		"https://somenode-ecommerse-server.onrender.com",
+		"https://aapan.shop/",
+	}
+
+	for _, url := range urls {
+		go func(url string) {
+			for {
+				GetReq(url)
+				fmt.Println("I will send it every 5 minutes")
+				time.Sleep(5 * time.Second)
+			}
+		}(url)
+	}
+
+	http.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Server is healthy")
+	})
+
+	http.ListenAndServe(":8080", nil)
+}
+
+func GetReq(url string) {
+	resp, err := http.Get(url)
+	if err != nil {
+		fmt.Println("Error:", err)
+		return
+	}
+	defer resp.Body.Close()
+
+	fmt.Println("Response Status:", resp.Status)
+}
